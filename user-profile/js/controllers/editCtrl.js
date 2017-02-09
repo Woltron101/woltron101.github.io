5bd678ddf01d810e97f03ac12a769e4a4e73d935
woltron101 <woltron101@gmail.com>
2017-02-09 18:41:06 +0200

    user-profile

--- user-profile/js/controllers/editCtrl.js
+++ user-profile/js/controllers/editCtrl.js
@@ -9,11 +9,11 @@
     editController.inject = ['$sessionStorage', '$FirebaseArray', '$firebaseObject', '$state'];
 
     function editController($sessionStorage, $FirebaseArray, $firebaseObject, $state) {
-        var vm = this;
-        vm.user = $sessionStorage.user
-        var ref = firebase.database().ref(),
+        var vm = this,
+            ref = firebase.database().ref(),
             user = $firebaseObject(ref);
-
+        vm.user = $sessionStorage.user;
+        vm.phoneValidationPattern = /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/g
         vm.save = function() {
             user.$loaded().then(function() {
                 $sessionStorage.user = vm.user;
